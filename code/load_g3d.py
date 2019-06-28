f0ca0cf56d48bef37471c03e1805fc460868f8c5
p0werHu <20161643@cqu.edu.cn>
2019-06-28 16:43:25 +0800

    Initial commit

---
+++ code/load_g3d.py
@@ -0,0 +1,10 @@
+#implemented by junfeng Hu
+#28/6/2019
+
+import os
+import scipy.io as scio
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Lie-LSTM
+##Introduction
+This is the trial section of the final idea which is applying a SO(3) based neural network to human or animal action 
+recognition or prediction.
